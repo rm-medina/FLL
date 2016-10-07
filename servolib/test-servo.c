4ca9de020f71b0a3f994a8dfd85aa21a2a6ab09a
Raquel Medina <raquel.medina.rodriguez@gmail.com>
2016-10-07 16:04:19 -0400

    servolib: fix test program arguments and add debugging statements.

--- servolib/test-servo.c
+++ servolib/test-servo.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include "servolib.h"
 
+static int cmd_enum;
 static const struct option options[] = {
 	{
 #define help_opt 0
@@ -44,43 +45,43 @@ static const struct option options[] = {
 		.flag = NULL,
 	},
 	{
-#define accel_limit_opt 5,
+#define accel_limit_opt 5
 		.name = "accel",
 		.has_arg = 1,
 		.flag = NULL,
 	},
 	{
-#define reset_opt 6,
+#define reset_opt 6
 		.name = "reset",
 		.has_arg = 0,
 		.flag = &cmd_enum,
 		.val = SERVOIO_RESET,
 	},
 	{
-#define check_opt 7,
+#define check_opt 7
 		.name = "check",
 		.has_arg = 0,
 		.flag = &cmd_enum,
 		.val = SERVOIO_CHECK,
 	},
 	{ 
-#define read_opt 8,
+#define read_opt 8		
 		.name = "read",
 		.has_arg = 0,
 		.flag = &cmd_enum,
 		.val = SERVOIO_READ,
 	},
 	{
-#define configure_opt 9,
+#define configure_opt 9
 		.name = "init",
 		.has_arg = 0,
 		.flag = &cmd_enum,
 		.val = SERVOIO_CONFIG,
 	},
 	{
-#define write_opt 10,
+#define write_opt 10
 		.name = "write",
-		.has_arg = 1,
+		.has_arg = 0,
 		.flag = &cmd_enum,
 		.val = SERVOIO_WRITE,
 	},
@@ -104,15 +105,18 @@ static void usage(void)
 
 int main(int argc, char *argv[])
 {
-	int opt, dev, chan, pos, acclim, speelim;
-	int dev = 0;
+	int opt,readerr;
+	int dev, pos, acclim, speelim;
+	int opt_index, do_reset, check_err, get_pos, do_conf, do_set,ret;
 	int chan = 5;
-  
+	do_reset = check_err = get_pos = do_conf = do_set = 0;
+	dev = pos = acclim = speelim=-1;
 	for (;;) {
 		opt_index = -1;
 		opt = getopt_long_only(argc, argv, "", options, &opt_index);
 		if (opt == EOF)
 			break;
+		fprintf(stderr, "--help this help: %d.\n", opt_index);
 		switch (opt_index) {
 		case help_opt:
 			usage();
@@ -125,12 +129,15 @@ int main(int argc, char *argv[])
 			break;
 		case position_opt:
 			pos = atoi(optarg);
+			fprintf(stderr, "user entered pos:%d.\n", pos);
 			break;
 		case speed_limit_opt:
 			speelim = atoi(optarg);
+			fprintf(stderr, "user entered speed limit:%d.\n", pos);
 			break;
 		case accel_limit_opt:
 			acclim = atoi(optarg);
+			fprintf(stderr, "user entered accel limit:%d.\n", pos);
 			break;
 		case reset_opt:
 			do_reset = 1;
@@ -153,36 +160,41 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if ((do_set) && (pos_opt != -1)) {
+	if ((do_set) && (get_pos != -1)) {
 		ret = servoio_set_pulse(dev, chan, pos);
 		fprintf(stderr, "test-servo set_pulse " \
-			"ttyACM%d channel %d returned 0x%x.\n");
+			"ttyACM%d channel %d pulse:%d ret 0x%x.\n",
+			dev, chan, pos, ret);
 	}
-	if ((do_set) && (acc_opt != -1)) {
-		servoio_set_accel(dev,chan, acclimit);
+	if ((do_set) && (acclim != -1)) {
+		ret = servoio_set_accel(dev,chan, acclim);
 		fprintf(stderr, "test-servo set_accel "			\
-			"ttyACM%d channel %d returned 0x%x.\n");
+			"ttyACM%d channel %d accel:%d ret 0x%x.\n",
+			dev, chan, acclim, ret);
 	}
-	if ((do_set) && (spee_opt != -1)) {
-		servoio_set_speed(dev,chan, speelimit);
+	if ((do_set) && (speelim != -1)) {
+		ret = servoio_set_speed(dev,chan, speelim);
 		fprintf(stderr, "test-servo set_speed " \
-			"ttyACM%d channel %d returned 0x%x.\n");
+			"ttyACM%d channel %d speed:%d ret 0x%x.\n",
+			dev, chan, speelim, ret);
 	}
 	if (get_pos) {
 		pos = servoio_get_position(dev, chan);
-		fprintf(stderr, "test-servo ttyACM%d channel %d pos:%d.\n");
+		fprintf(stderr, "test-servo ttyACM%d channel %d pos:%d.\n",
+			dev, chan, pos);
 	}
-	if ((do_conf) && (pos_opt != -1) && (acc_opt != -1) &&
-	    (spee_opt != -1)) {
-		servoio_configure(dev, chan, pos, speelimit, acclimit);
+	if ((do_conf) && (pos != -1) && (acclim != -1) &&
+	    (speelim != -1)) {
+		ret = servoio_configure(dev, chan, pos, speelim, acclim);
 		fprintf(stderr, "test-servo ttyACM%d channel %d returned " \
-			"0x%x.\n");
+			"0x%x.\n", dev, chan, ret);
 	}
 	if (do_reset)
 		servoio_all_go_home(dev);
 	if (check_err) {
 		readerr = servoio_get_any_error(dev);
-		fprintf(stderr, "servoio err %d on dev ttyACM%d.\n");
+		fprintf(stderr, "servoio err %d on dev ttyACM%d.\n",
+			readerr, dev);
 	}
 	return 0;
 }
