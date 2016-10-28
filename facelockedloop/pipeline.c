09a657d69a28917f9753454bc50d181780213835
Raquel Medina <raquel.medina.rodriguez@gmail.com>
2016-10-28 05:09:31 -0400

    pipeline: complete pipeline skeleton.

--- facelockedloop/pipeline.c
+++ facelockedloop/pipeline.c
@@ -4,7 +4,135 @@
 #include "imgproc/imgproc_c.h"
 #include "objdetect/objdetect.hpp"
 
-int capture_process(int vindex, enum object_detector_t cdt, int scale)
+int stage_up(struct stage *stg, const struct stage_params *p,
+	     struct stage_ops *o, struct pipeline *pipe)
+{
+	pthread_attr_t attr;
+
+	stg->params = p;
+	stg->ops = o;
+	stg->pipeline = pipe;
+	timespec_zero(&stg->duration);
+	timespec_zero(&stg->stats.lastrun);
+	stg->stats.ofinterest = 0;
+	stg->stats.persecond = 0;
+
+	if (stg->params.nth_stage < PIPELINE_MAX_STAGE)
+		stg->next = &(pipe->stages[stg->params.nth_stage]);
+	else
+		stg->next = NULL; /* end of pipeline */
+
+	sem_init(&stg->nowait);
+	sem_init(&stg->done);
+	mutex_init(&stg->lock);
+	pthread_cond_init(&stg->sync, NULL);
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	pthread_create(&stg->worker, &attr, stage_worker, stg);
+	ptrhead_attr_destroy(&attr);
+	
+	return 0;
+}
+
+static void *stage_worker(void *arg)
+{
+	struct timespec start, stop;
+	struct stage *step = arg;
+	int ret;
+	
+	for (;;)
+	{
+		//wait_for_previous_step
+		ret = sem_wait(&step->nowait);
+		if (ret)
+			printf("step %d wait error %d./n",
+			       step->params.nth_stage, ret);
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		ret = step->ops->run(step);
+		clock_gettime(CLOCK_MONOTONIC, &stop);
+		timespec_sub(&step->duration, &stop, &start);
+	}
+	if (ret < 0)
+		printf("step %d failed./n", step->params.nth_stage);
+
+	printf("step %d performance: nofinterest=%d./n",
+	       step->stats.ofinterest);
+
+	//post to next step
+	sem_post(&step->done);
+	return NULL;
+}
+
+void stage_go(struct stage *stg)
+{
+	sem_post(&stg->nowait);
+}
+
+void stage_wait(struct stage *stg)
+{
+	ret = sem_wait(&stg->done);
+	if (ret)
+		printf("%s: step %d wait error %d./n",
+		       __func__, step->params.nth_stage, ret);
+
+}
+
+int stage_passit(struct stage *stg, void *it)
+{
+	struct stage *next = stg->next;
+
+	if (next) {
+		mutex_lock(&next->lock);
+		next->params.data_in = it;
+		pthread_cond_signal(next->sync);
+		mutex_unlock(&next->lock);
+	}
+
+}
+
+int stage_processit(struct stage *stg, void **it)
+{
+	mutex_lock(&stg->lock);
+	while (stg->params->data_in == NULL)
+		pthread_cond_wait(&stg->sync, &stg->lock);
+
+	mutex_unlock(&stg_lock);
+	it = stg->params->data_in;
+}
+	
+void stage_down(struct stage *stg)
+{
+	pthread_cancel(stg->worker);
+	pthread_join(stg->worker, NULL);
+	pthread_cond_destroy(&stg->sync);
+	mutex_destroy(&stg->lock);
+	sem_destroy(&stg->nowait);
+	sem_destroy(&stg->done);
+}
+
+void stage_printstats(struct stage *stg)
+{
+	return 0;
+}
+
+void pipeline_init(struct pipeline *pipe)
+{
+	pipe->count = 0;
+	pipe->status = 0;
+	memset(pipe->stgs, 0, PIPELINE_MAX_STAGE * (sizeof(struct stage)));
+}
+
+int pipeline_register(struct pipeline *pipe, struct stage *stg)
+{
+	if (!stg)
+		return -EINVAL;
+	
+	pipe->stgs[stg->params->nth_stage] = stg;
+	++(pipe->count);
+	return 0;	
+}
+
+int pipeline_run(int vindex, enum object_detector_t cdt, int scale)
 {
 	IplImage* srcframe, *dstframe;
 	CvSeq* faces;
@@ -38,6 +166,7 @@ int capture_process(int vindex, enum object_detector_t cdt, int scale)
 	return ret;
 }
 
+
 #else
 
 int capture_process(enum capture_detector_t cdt, int scale)
@@ -56,3 +185,9 @@ int capture_read_locations(struct store_box *faceset)
 	return -ENODEV;
 }
 #endif /*HAVE_OPENCV2*/
+
+
+int pipeline_getcount(struct pipeline *pipe)
+{
+	return pipe->count;
+}
--- facelockedloop/pipeline.h
+++ facelockedloop/pipeline.h
@@ -1,30 +1,76 @@
+#ifndef __PIPELINE_H_
+#define __PIPELINE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PIPELINE_MAX_STAGE 3
+#define CAPTURE_STAGE 0
+#define DETECTIGON_STAGE 1
+#define TRACKING_STAGE 2
+
 struct detector;
 struct pipeline {
-	struct list stages;
+	struct stage stgs[PIPELINE_MAX_STAGE];
+	int count;
 	int status;
 };
 
 void pipeline_init(struct pipeline *pipe);
-void pipeline_teardown(struct pipeline* pipe);
-int pipeline_run(struct pipeline* pipe);
-
+int pipeline_register(struct pipeline *pipe, struct stage *stg);
+void pipeline_teardown(struct pipeline *pipe);
+int pipeline_run(struct pipeline *pipe);
+int pipeline_printstats(struct pipeline *pipe);
+int pipeline_getcount(struct pipeline *pipe);
 
 struct stage_ops {
-	int (*initialize)(struct stage* step);
-	int (*run)(struct stage* step);
-	void (*teardown)(struct stage* step);
-	int (*get_numbers)(struct stage* step);
+	int (*up)(struct stage *stg, const struct stage_params *p,
+		  struct stage_ops *o, struct pipeline *pipe);
+	int (*run)(struct stage *stg);
+	void (*down)(struct stage *stg);
+	void (*go)(struct stage *stg);
+	void (*wait)(struct stage *stg);
+	int (*passit)(struct stage *stg, void *it);
+	int (*processit)(struct stage *stg, void **it);
+	int (*count)(struct stage *step);
+	void (*printstats)(struct stage *step);
+};
+
+struct stage_params {
+	int nth_stage;
+	void *data_in;
+	void *data_out;
 };
 
 struct stage {
+	struct stage *self;
 	struct stage_ops *ops;
 	struct stage_params *params;
 	struct pipeline *pipeline;
-	struct holder next;
+	struct stage* next;
 	struct timespec duration;
+	pthread_t worker;
+	sem_t *nowait;
+	sem_t *done;
 	struct performance {
-		struct timespec lastout;
-		unsigned long nframes;
-		unsigned long framesps;
+		struct timespec lastrun;
+		unsigned long nofinterest;
+		unsigned long persecond;
 	} stats;
 };
+
+int stage_up(struct stage *stg, const struct stage_params *p,
+	     struct stage_ops *o, struct pipeline *pipe);
+int stage_down(struct stage *stg);	
+void stage_go(struct stage *stg);
+void stage_wait(struct stage *stg); 
+int stage_passit(struct stage *stg, void *it);
+int stage_processit(struct stage *stg, void **it);
+void stage_printstats(struct stage *stg);
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __PIPELINE_H_ */
