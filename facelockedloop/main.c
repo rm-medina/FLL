82b3deaa0c115c05fb99769f83ff57192c538842
Raquel Medina <raquel.medina.rodriguez@gmail.com>
2016-10-29 09:23:51 -0400

    fll/main: use new infraestructure for running full pipeline.

--- facelockedloop/main.c
+++ facelockedloop/main.c
@@ -13,12 +13,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
-#include "servolib.h"
+
+#include "pipeline.h"
+#include "capture.h"
+#include "detect.h"
+#include "track.h"
+#include "time_utils.h"
 
 extern const char *fll_version_name;
 #define FLL_MAX_SERVO_COUNT SERVOLIB_MAX_SERVO_COUNT
 #define FLL_SERVO_COUNT 2
-struct pipeline pipe;
+
+struct pipeline fllpipe;
 
 
 static int with_output;
@@ -48,63 +54,62 @@ static const struct option options[] = {
 		.has_arg = 1,
 		.flag = NULL,
 	},
+	{
+#define algrthm_opt 4
+		.name = "algorithm",
+		.has_arg = 1,
+		.flag = NULL,
+	},
+	{
+#define trackdev_opt 5
+		.name = "servodev",
+		.has_arg = 1,
+		.flag = NULL,
+	},
+	{
+#define trackpan_opt   6
+		.name = "panchannel",
+		.has_arg = 1,
+		.flag = NULL,
+	},
+	{
+#define tracktilt_opt   7
+		.name = "tiltchannel",
+		.has_arg = 1,
+		.flag = NULL,
+	},
 };
 
 static void usage(void)
 {
 	fprintf(stderr, "usage: fll  <options>, with:\n");
-	fprintf(stderr, "--xmlfile=<filepath/filename> 		specifies detector config file (default: ~/cascade.xml)\n");
-	fprintf(stderr, "--output[=<file-tmpl>] 		dump output data (default: discard)\n");
-	fprintf(stderr, "--video[=<camera-index>] 		specifies which camera to use (default: any camera)\n");
-	fprintf(stderr, "--help                 		this help\n");
+	fprintf(stderr, "--xmlfile=<filepath/filename> 		"
+		"specifies detector config file (default: ~/cascade.xml)\n");
+	fprintf(stderr, "--output[=<file-tmpl>] 		"
+		"dump output data (default: discard)\n");
+	fprintf(stderr, "--video[=<camera-index>] 		"
+		"specifies which camera to use (default: any camera)\n");
+	fprintf(stderr, "--algorithm[=<haar>|<lsvm>]            "
+		"select which detection algorithm to use (default: haar(\n");
+	fprintf(stderr, "--servodevnode=<dev-node-index> 		"
+		"specifies the servos device control node (default: 0)\n");
+	fprintf(stderr, "--panchannel[=<channel-index>] 		"
+		"specifies which channel the pan servo is connected to"
+		"(default: 2)\n");
+	fprintf(stderr, "--tiltchannel[=<channel-index>] 		"
+		"specifies which channel the tilt servo is connected to"
+		"(default: 5)\n");
+	fprintf(stderr, "--help                 		"
+		"this help\n");
 }
 
 
 /*helper function*/
-static int timeval_subtract (struct timeval *result,
-			     struct timeval *x,
-			     struct timeval *y) __attribute__((unused));
 
-static int timeval_subtract (struct timeval *result,
-			     struct timeval *x, struct timeval *y)
-{
-	/* Perform the carry for the later subtraction by updating y. */
-	if (x->tv_usec < y->tv_usec) {
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
-	/* Compute the time remaining to wait.
-	   tv_usec is certainly positive. */
-	result->tv_sec = x->tv_sec - y->tv_sec;
-	result->tv_usec = x->tv_usec - y->tv_usec;
-
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
-}
-
-static void timespec_substract(struct timespec *const diff,
-			       const struct timespec *const start,
-			       const struct timespec *const end)
-{
-	if ((end->tv_nsec-start->tv_nsec)<0) {
-		diff->tv_sec = end->tv_sec-start->tv_sec-1;
-		diff->tv_nsec = 1000000000+end->tv_nsec-start->tv_nsec;
-	} else {
-		diff->tv_sec = end->tv_sec-start->tv_sec;
-		diff->tv_nsec = end->tv_nsec-start->tv_nsec;
-	}
-}
 
 int main(int argc, char *const argv[])
 {
-	const char *outfile, *xmlfile;
+	char *outfile, *xmlfile;
 	int video = 0;
 	struct timespec start_time, stop_time, duration;
 	int pos[FLL_MAX_SERVO_COUNT] =
@@ -113,16 +118,19 @@ int main(int argc, char *const argv[])
 		{ [0 ... FLL_MAX_SERVO_COUNT -1] = -1};
 	int accel[FLL_MAX_SERVO_COUNT] =
 		{ [0 ... FLL_MAX_SERVO_COUNT -1] = -1};
-	struct servo_params servo_ __attribute__((unused));
+	struct tracker_params servo_params;
 	struct imager_params camera_params;
 	struct detector_params algorithm_params;
 	struct imager camera;
 	struct detector algorithm;
 	struct tracker servo;
-	
-	int lindex, c, i;
+	enum object_detector_t dtype = CDT_HAAR;
+	int lindex, c, i, l, ret, panchannel, tiltchannel, servodevnode;
+	servodevnode = 0;
 	outfile = NULL;
 	xmlfile = NULL;
+	panchannel = 5;
+	tiltchannel = 2;
 	
 	/* get local configurations */
 	for (;;) {
@@ -142,6 +150,20 @@ int main(int argc, char *const argv[])
 			break;
 		case camera_opt:
 			video = atoi(optarg);
+			break;
+		case algrthm_opt:
+			if (optarg && strncmp(optarg, "lsvm",4) == 0)
+				dtype = CDT_LSVM;
+			break;
+		case trackdev_opt:
+			servodevnode = atoi(optarg);
+			break;
+		case trackpan_opt:
+			panchannel = atoi(optarg);
+			break;
+		case tracktilt_opt:
+			tiltchannel = atoi(optarg);
+			break;
 		default:
 			usage();
 			exit(1);
@@ -152,14 +174,16 @@ int main(int argc, char *const argv[])
 	if (outfile != NULL)
 		printf("output data:%s.\n", outfile);
 
-	pipeline_init(pipe);
+	pipeline_init(&fllpipe);
 	/* first stage */
-	camera_params.name = sscanf("FLL cam%d", video);
+	ret = asprintf(&camera_params.name, "FLL cam%d", video);
+	if (ret)
+		goto terminate;
+	
 	camera_params.vididx = video;
-	camera_params.camidx = 0;
 	camera_params.frame = NULL;
 	camera_params.videocam = NULL;
-	ret = capture_initialize(&camera, &camera_params, &pipe);
+	ret = capture_initialize(&camera, &camera_params, &fllpipe);
 	if (ret) {
 		printf("capture init ret:%d.\n", ret);
 		goto terminate;
@@ -171,22 +195,25 @@ int main(int argc, char *const argv[])
 	algorithm_params.dstframe = NULL;
 	algorithm_params.algorithm = NULL;
 	algorithm_params.scratchbuf = NULL;
-	ret = detect_initialize(&algorithm, &algorithm_params, &pipe);
+	ret = detect_initialize(&algorithm, &algorithm_params, &fllpipe);
 	if (ret) {
 		printf("detection init ret:%d.\n", ret);
 		goto terminate;
 	}
 	/* third stage */
-	servo_params.panhome = 0;
-	servo_params.tilthome = 0;
-	ret = track_initialize(&servo , &servo_params, &pipe);
+	servo_params.pan_tgt = 0;
+	servo_params.tilt_tgt = 0;
+	servo_params.dev = servodevnode;
+	servo_params.pan_params.channel = panchannel;
+	servo_params.tilt_params.channel = tiltchannel;
+	ret = track_initialize(&servo , &servo_params, &fllpipe);
 	if (ret) {
 		printf("tracking init ret:%d.\n", ret);
 		goto terminate;
 	}
 
-	ret =pipeline_getstages(&pipe);
-	if (ret != MAX_PIPELINE_STAGES) {
+	ret =pipeline_getcount(&fllpipe);
+	if (ret != PIPELINE_MAX_STAGE) {
 		printf("missing stages for fll, only %d present.\n", ret);
 		goto terminate;
 	}
@@ -197,15 +224,16 @@ int main(int argc, char *const argv[])
 
 	clock_gettime(CLOCK_MONOTONIC, &start_time);
 	for (l=0; ret <= 0; l++)  {
-		ret = pipeline_run(&pipe);
+		ret = pipeline_run(&fllpipe);
 		if (ret) {
 			printf("Cannot run FLL, ret:%d.\n", ret);
 			break;
 		}
-
-	pipeline_teardown(&pipe);
+	}
+terminate:
+	pipeline_teardown(&fllpipe);
 	clock_gettime(CLOCK_MONOTONIC, &stop_time);
-	timespec_substract(&duration, &start_time, &stop_time);
+	timespec_substract(&duration, &stop_time, &start_time);
 	printf("duration->  %lds %ldns .\n", duration.tv_sec , duration.tv_nsec );
 	return 0;
 }
