e8963084187b54227d04f8840147a954d03b0362
Raquel Medina <raquel.medina.rodriguez@gmail.com>
2017-03-27 11:19:03 -0400

    FLL: cleanup build files.

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,9 @@ aclocal --install -I m4 &&
     autoconf &&
     automake --add-missing --copy &&
 
-#x86_64:
-#    ./configure --with-opencv=/usr/ --enable-debug=full --prefix=/home/rmedina/FLL/target/x86_64/usr/local/fll/ "$@"
+x86_64:
+    ./configure --with-opencv=/usr/ --enable-debug=full --prefix=/home/rmedina/FLL/target/x86_64/usr/local/fll/ "$@"
 #    ./configure --enable-doc --with-opencv=/usr/ "$@"
 
 #arm:
-	./configure --host=arm-linux-gnueabi --build=x86_64 --with-opencv=/home/rmedina/FLL/target/arm/usr/local/opencv/ --enable-debug=full --prefix=/home/rmedina/FLL/target/arm/usr/local/fll/ "$@"
+#	./configure --host=arm-linux-gnueabi --build=x86_64 --with-opencv=/home/rmedina/FLL/target/arm/usr/local/opencv/ --enable-debug=full --prefix=/home/rmedina/FLL/target/arm/usr/local/fll/ "$@"
